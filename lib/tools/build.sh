6205c1766efcf26e56c2fde93c968f8233ca6342
Nika <nika.begiashvili@gmail.com>
2018-10-13 16:36:47 +0400

    Add sample archives Add lzma support

--- lib/tools/build.sh
+++ lib/tools/build.sh
@@ -1,6 +1,7 @@
 emcc ../wrapper/main.c -I /home/ubuntu/libarchive-3.3.2/libarchive -o ../build/main.o
 
-emcc ../build/main.o ../build/libarchive.a -o ../build/libarchive.js -s USE_ZLIB=1 -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS='["_get_version","_archive_open","_get_next_entry","_get_filedata","_archive_close","_archive_entry_filetype","_archive_entry_pathname","_archive_entry_pathname_utf8","_archive_entry_size","_archive_error_string","_free"]'
+emcc ../build/main.o ../build/libarchive.a /usr/local/lib/liblzma.so -o ../build/libarchive.js -s USE_ZLIB=1 -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 \
+-s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS='["_get_version","_archive_open","_get_next_entry","_get_filedata","_archive_close","_archive_entry_filetype","_archive_entry_pathname","_archive_entry_pathname_utf8","_archive_entry_size","_archive_error_string","_free"]'
 
 cp ../build/libarchive.js ../../src/webworker/wasm-gen/
 cp ../build/libarchive.wasm ../../src/webworker/wasm-gen/
--- lib/tools/libarchive_configure.sh
+++ lib/tools/libarchive_configure.sh
@@ -4,8 +4,10 @@ export LDFLAGS="-L/usr/local/lib -lz"
 
 emconfigure ./configure --enable-static --disable-shared --enable-bsdtar=static --enable-bsdcat=static --enable-bsdcpio=static --enable-posix-regex-lib=libc \
 --disable-xattr --disable-acl \
---without-bz2lib --without-iconv --without-libiconv-prefix --without-lz4 --without-lzma --without-lzo2 --without-cng \
+--without-cng \
 --without-nettle --without-openssl --without-xml2 --without-expat --libdir=/home/ubuntu/libarchive-3.3.2/target
+
+#--without-bz2lib --without-iconv --without-libiconv-prefix --without-lz4 --without-lzma --without-lzo2
 
 emmake make
 
