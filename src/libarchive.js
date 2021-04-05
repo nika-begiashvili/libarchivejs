0664bccc826a5ecbfeb2acf816a5fc05ab7e2576
Alexandre Geissmann <alex@la-bonaventure.com>
2021-04-05 08:55:35 -0400

    debug

--- src/libarchive.js
+++ src/libarchive.js
@@ -132,6 +132,7 @@ export class Archive{
 
         return this._postMessage({type: 'EXTRACT_SINGLE_FILE', target: target}, 
             (resolve,reject,msg) => {
+                console.log(msg, target);
                 if( msg.type === 'FILE' && msg.target === target){
                     const file = new File([msg.entry.fileData], msg.entry.fileName, {
                         type: 'application/octet-stream'
